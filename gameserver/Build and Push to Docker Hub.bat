92b6f747e1326ec3264d2e070f49b013e7ef2501
Daan Sleven <Daan.Sleven@student.pxl.be>
2021-03-23 12:40:20 +0100

    Added project to git :)

---
+++ gameserver/Build and Push to Docker Hub.bat
@@ -0,0 +1,3 @@
+docker image build -t daansleven/mirror_kcp_server:latest .
+docker image push daansleven/mirror_kcp_server:latest
+pause
---
+++ gameserver/Run locally.bat
@@ -0,0 +1,2 @@
+docker run --network host -p 7777:7777 --name mirror-kcp-server daansleven/mirror_kcp_server:latest
+pause
